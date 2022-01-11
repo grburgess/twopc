193887900112e429ec122a1ff79399467b8e3223
grburgess <jmichaelburgess@gmail.com>
2022-01-11 11:48:10 +0100

    updates

--- twopc/__init__.py
+++ twopc/__init__.py
@@ -1,6 +1,7 @@
-from .ppc import PPCDetector, compute_postpc, compute_priorpc,PPC
+from .ppc import PPCDetector, compute_postpc, compute_priorpc, PPC
 
 
 from ._version import get_versions
-__version__ = get_versions()['version']
+
+__version__ = get_versions()["version"]
 del get_versions
